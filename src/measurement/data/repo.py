14a28bd8fda33c6cfc36b7c74c7fac1ad60b3bae
David Swords <dswords@akvagroup.com>
2023-05-20 19:11:45 +0200

    Added logic to retrieve measurement from database.

--- src/measurement/data/repo.py
+++ src/measurement/data/repo.py
@@ -1,22 +1,28 @@
-from datetime import datetime
 from typing import List
 
 from src.measurement.domain.entity import MeasurementEntity
+from src.measurement.data.dao import MeasurementDAO
 
 
 class MeasurementRepo:
     @staticmethod
-    def retrieve_by_country(country: str) -> List[MeasurementEntity]:
-        return [
-            MeasurementEntity(
-                id_="216bb524",
-                created_at=datetime.utcnow(),
-                updated_at=datetime.utcnow(),
-                archived=False,
-                recorded_at=datetime.utcnow(),
-                city="Trondheim",
-                country="NO",
-                pollutant="PM2.5",
-                value=2.5,
-            ),
-        ]
+    def _to_entity(dao: MeasurementDAO) -> MeasurementEntity:
+        return MeasurementEntity(
+            id_=dao.id_,
+            created_at=dao.created_at,
+            updated_at=dao.updated_at,
+            archived=dao.archived,
+            recorded_at=dao.recorded_at,
+            city=dao.city,
+            country=dao.country,
+            pollutant=dao.pollutant,
+            value=dao.value,
+        )
+
+    def retrieve_by_country(self, country: str) -> List[MeasurementEntity]:
+        daos = MeasurementDAO.query.filter(
+            MeasurementDAO.country == country and
+            MeasurementDAO.archived is False
+        ).all()
+
+        return [self._to_entity(dao) for dao in daos]
