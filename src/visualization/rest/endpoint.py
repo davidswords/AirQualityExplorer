687808e695798dffb4f66d1889726e697240c342
David Swords <dswords@akvagroup.com>
2023-05-22 19:18:40 +0200

    Cleaned up the implementation of the pollutant map.

--- src/visualization/rest/endpoint.py
+++ src/visualization/rest/endpoint.py
@@ -14,29 +14,34 @@ def map():
     return render_template("map.html", map=folium_map._repr_html_())
 
 
-@app.route("/map/<pollutant>", methods=["GET"])
-def pollutant_map(pollutant: str):
+def get_measurements(pollutant):
     # Get measurements for the pollutant
     service = MeasurementService()
     entities = service.retrieve_by_pollutant(pollutant=pollutant)
+    return entities
 
+
+def calculate_values(entities):
     # Calculate min and max values for the colormap normalization
-    values = []
-    for entity in entities:
-        if entity.latitude and entity.longitude and entity.value >= 0:
-            values.append(entity.value)
+    values = [
+        entity.value
+        for entity in entities
+        if entity.latitude and entity.longitude and entity.value >= 0
+    ]
     min_value, max_value = min(values), max(values)
+    return min_value, max_value, values
 
-    app.logger.info(min_value)
-    app.logger.info(max_value)
-    app.logger.info(values)
 
+def create_colormap(min_value, max_value, pollutant):
     # Create a colormap for the data
     colormap = branca.colormap.LinearColormap(
         colors=["green", "yellow", "red"], vmin=min_value, vmax=max_value
     ).to_step(5)
     colormap.caption = f"Air quality for {pollutant}"
+    return colormap
 
+
+def create_map(entities, colormap):
     # Create a map centered at some location
     m = folium.Map(location=[62.278600, 12.340200], zoom_start=6)
 
@@ -45,7 +50,7 @@ def pollutant_map(pollutant: str):
         if entity.latitude and entity.longitude and entity.value >= 0:
             marker_color = colormap(entity.value)
             folium.CircleMarker(
-                location=[entity.latitude, entity.longitude],
+                location=(entity.latitude, entity.longitude),
                 radius=10,
                 popup=f"{entity.city}, {entity.country}: {entity.value}",
                 color=marker_color,
@@ -55,9 +60,15 @@ def pollutant_map(pollutant: str):
 
     # Add the colormap to the map
     m.add_child(colormap)
+    return m
 
+
+@app.route("/map/<pollutant>", methods=["GET"])
+def pollutant_map(pollutant: str):
+    entities = get_measurements(pollutant)
+    min_value, max_value, values = calculate_values(entities)
+    colormap = create_colormap(min_value, max_value, pollutant)
+    m = create_map(entities, colormap)
     # Render the map to HTML and return it
     map_html = m._repr_html_()
-
     return render_template("map.html", map=map_html, pollutant=pollutant)
-
