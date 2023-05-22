03a5b0886356bd7b8464688283ecb6a975c33428
David Swords <dswords@akvagroup.com>
2023-05-22 23:50:15 +0200

    Added openapi spec to the health endpoint.

--- src/health/rest/endpoint.py
+++ src/health/rest/endpoint.py
@@ -5,4 +5,14 @@ from flask import jsonify
 
 @app.route("/health")
 def health():
+    """
+    ---
+    tags:
+      - Health
+    responses:
+      200:
+        description: The status of the health check.
+        schema:
+          type: string
+    """
     return jsonify("healthy!"), 200
